7cbb7156873471112a8b5f8cf44575d7f781ef0d
nabilweq <nabeeltkanr@gmail.com>
2022-11-28 23:01:19 +0530

    create prescription

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 
 router.use('/appointments', require('./appointment'));
 router.use('/doctors', require('./doctor'));
-router.use('/forms', require('./form'))
+router.use('/forms', require('./form'));
+router.use('/presc', require('./presc'))
 //router.use('/admin', './admin');
 
 module.exports = router;
