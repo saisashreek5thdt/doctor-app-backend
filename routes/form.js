1f539aeb542ead0146352fc429233f1b5799f999
nabilweq <nabeeltkanr@gmail.com>
2022-12-13 23:16:26 +0530

    submit form

--- routes/form.js
+++ routes/form.js
@@ -5,5 +5,6 @@ const form = require('../controllers/form');
 const { checkPatient } = require('../middleware/auth');
 
 router.get('/get-all', form.getAll);
+router.post('/submit-form', form.submitForm);
 
 module.exports = router;
